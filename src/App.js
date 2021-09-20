853011482be3ed2202cdb4c423bd13ac35633714
marcelkida <47465692+marcelkida@users.noreply.github.com>
2021-09-19 23:48:14 -0400

    ini

--- src/App.js
+++ src/App.js
@@ -1,45 +1,112 @@
-import React, {useState, useRef, useEffect} from "react";
+import React, { useEffect, useContext, useState, useRef } from 'react'
+import Button from './components/Button'
 import TodoList from './TodoList'
+import CountdownAnimation from './components/CountdownAnimation'
+import SetPomodoro from './components/SetPomodoro'
+import { SettingsContext } from './context/SettingsContext'
 import {v4 as uuidv4} from 'uuid'
-import Pomodoro from './Pomodoro'
+import './index.css';
+
 
 const LOCAL_STORAGE_KEY = 'todoApp.todos'
-function App() {
+const App = () => {
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
 
-  useEffect(() =>{
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    if (storedTodos) setTodos(storedTodos)
-  }, [])
+  const {
+    pomodoro,
+    executing,
+    startAnimate,
+    children,
+    startTimer,
+    pauseTimer,
+    updateExecute,
+    setCurrentTimer,
+    SettingsBtn,
+    buttonText } = useContext(SettingsContext)
 
-  useEffect(() =>{
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  }, [todos])
+    useEffect(() => {updateExecute(executing)}, [executing, startAnimate])
 
-function toggleTodo(id){
-  const newTodos = [...todos]
-  const todo = newTodos.find(todo => todo.id === id)
-  todo.complete = !todo.complete
-  setTodos(newTodos)
-}
-
-  function handleAddTodo(e){
-      const name = todoNameRef.current.value
-      if (name === '') return
-      setTodos(prevTodos => {
-        return [...prevTodos, { id: uuidv4(), name: name, complete: false}]
-      })
+    useEffect(() =>{
+      const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+      if (storedTodos) setTodos(storedTodos)
+    }, [])
+  
+    useEffect(() =>{
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+    }, [todos])
+  
+  function toggleTodo(id){
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
   }
-  return ( 
-    <>
-      <Pomodoro/>
-      <TodoList todos={todos} toggleTodo={toggleTodo}/>
+  
+    function handleAddTodo(e){
+        const name = todoNameRef.current.value
+        if (name === '') return
+        setTodos(prevTodos => {
+          return [...prevTodos, { id: uuidv4(), name: name, complete: false}]
+        })
+    }
+
+  return (
+    <div className="container">
+      <h1>pomotomo</h1>
+      {pomodoro !== 0 ?
+      <>
+      <Button title="settings" _callback={SettingsBtn} />
+        <ul className="labels">
+          <li>
+            <Button 
+              title="work" 
+              activeClass={executing.active === 'work' ? 'active-label' : undefined} 
+              _callback={() => setCurrentTimer('work') } 
+              //onClick= {document.documentElement.style.setProperty('--color', '#9974ba')}
+            />
+          </li>
+          <li>
+            <Button 
+              title="short break" 
+              activeClass={executing.active === 'short' ? 'active-label' : undefined} 
+              _callback={() => setCurrentTimer('short')}
+              //onClick= {document.documentElement.style.setProperty('--color', '#74BA7B')} 
+            />
+          </li>
+          <li>
+            <Button 
+              title="long break" 
+              activeClass={executing.active === 'long' ? 'active-label' : undefined} 
+              _callback={() => setCurrentTimer('long')} 
+              //onClick= {document.documentElement.style.setProperty('--color', '#7490ba')}
+            />
+          </li>
+        </ul>
+        
+        <div className="timer-container">
+          <div className="time-wrapper">
+              <CountdownAnimation
+                key={pomodoro} 
+                timer={pomodoro} 
+                animate={startAnimate}
+              >
+                {children}
+              </CountdownAnimation>
+          </div>
+        </div>
+        <div className="button-wrapper">
+          <Button title="start" activeClass={!startAnimate ? 'active' : undefined} _callback={startTimer} onClick/>
+          <Button title="pause" activeClass={startAnimate ? 'active' : undefined} _callback={pauseTimer} />
+        </div>
+        <TodoList todos={todos} toggleTodo={toggleTodo}/>
       <input ref={todoNameRef} type="text"/>
-      <button onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={handleAddTodo}>add todo</button>
       <div>{todos.filter(todo => !todo.complete).length} left to do</div>
-    </>
-  );
+    
+      </> : <SetPomodoro />}
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/SetPomodoro.js
@@ -0,0 +1,62 @@
+import React, { useContext, useState } from 'react'
+import { SettingsContext } from '../context/SettingsContext'
+
+const SetPomodoro = () => {
+
+    const [newTimer, setNewTimer] = useState({
+        work: 3,
+        short: 1,
+        long: 2,
+        active: 'work'
+    })
+
+    const {updateExecute} = useContext(SettingsContext)
+
+    const handleChange = input => {
+        const {name, value} = input.target
+        switch (name) {
+            case 'work':
+                setNewTimer({
+                    ...newTimer,
+                    work: parseInt(value)
+                })
+                break;
+            case 'shortBreak':
+                setNewTimer({
+                    ...newTimer,
+                    short: parseInt(value)
+                })
+                break;
+            case 'longBreak':
+                setNewTimer({
+                    ...newTimer,
+                    long: parseInt(value)
+                })
+                break;
+        }
+    }
+    const handleSubmit = e => {
+        e.preventDefault()
+        updateExecute(newTimer)
+    }
+    return (
+        <div className="form-container">
+            <form noValidate onSubmit={handleSubmit}>
+                <div className="input-wrapper">
+                    <div className="input-title">work
+                    <input className="input" type="number" name="work" onChange={handleChange} value={newTimer.work} />
+                    </div>
+                    <div className="input-title">short
+                    <input className="input" type="number" name="shortBreak" onChange={handleChange} value={newTimer.short} />
+                    </div>
+                    <div className="input-title">long
+                    <input className="input" type="number" name="longBreak" onChange={handleChange} value={newTimer.long} />
+                    </div>
+                </div>
+                <button type='submit'>Set Timer</button>
+            </form>
+        </div>
+    )
+}
+
+export default SetPomodoro
